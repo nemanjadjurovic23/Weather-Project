2c650d08ac238041c2a42ffcc4f029bbd0bc0453
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-05-07 12:52:12 +0200

    * added UserWeatherSeeder

---
+++ database/seeders/UserWeatherSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CityTemperatures;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserWeatherSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $city = $this->command->getOutput()->ask('What is your city?');
+        if($city == null) {
+            $this->command->getOutput()->error('You have not entered a city name');
+        }
+
+        $cityTemperature = $this->command->getOutput()->ask('What is your city temperature?');
+        if($cityTemperature == null) {
+            $this->command->getOutput()->error('You have not entered a city temperature');
+        }
+        CityTemperatures::create([
+            'city' => $city,
+            'temperature' => $cityTemperature,
+        ]);
+
+        $this->command->getOutput()->info("You have successfully added city $city and a temperature $cityTemperature");
+
+    }
+}
