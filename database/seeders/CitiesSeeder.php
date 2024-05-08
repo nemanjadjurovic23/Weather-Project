26e89d43807b4d94fd2d318e332ddd32ee710ff9
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-05-08 16:13:40 +0200

    * Cities table and seeder added

---
+++ database/seeders/CitiesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cities;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CitiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->command->getOutput()->progressStart(100);
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            Cities::create([
+                'name' => $faker->city,
+            ]);
+            $this->command->getOutput()->progressAdvance();
+        }
+
+        $this->command->getOutput()->progressFinish();
+    }
+}
