f74e44dd6b84db7d860d8b22ccd2696831215761
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-05-09 19:04:05 +0200

    * 12 domaci: cities table, model updated

--- database/migrations/2024_05_09_103906_create_weather_table.php
+++ database/migrations/2024_05_09_103906_create_weather_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('city_id');
             $table->integer('temperature');
             $table->timestamps();
+
+            $table->foreign('city_id')->references('id')->on('cities');
         });
     }
 
